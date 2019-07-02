6b61138a80f00ce77f5a774ca6c415a5821cea91
Rainbow Li <52395053+Rainbowliff@users.noreply.github.com>
2019-07-02 13:13:49 +0800

    Add files via upload
    This is a program that I made that can convert any number of seconds to hours and minutes! enjoy~

---
+++ Second converter.py
@@ -0,0 +1,12 @@
+print("What is your name?")
+name = input()
+
+print ("How many seconds would you like to convert? Please enter")
+second = input()
+
+z = int(second/60)
+hour = int(z/60)
+minutes = int(z - hour*60)
+
+print ("Hi", name, second, "seconds is equal to", hour, 'hour(s) and', minutes, 'minute(s)')
+
