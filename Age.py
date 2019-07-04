217864106473d5f84b5c3e954fe9fd765747a473
Rainbow Li <52395053+Rainbowliff@users.noreply.github.com>
2019-07-04 11:53:56 +0800

    Add files via upload
    Caulculates your age ~

---
+++ Age.py
@@ -0,0 +1,10 @@
+print ("enter your age:")
+year = input()
+
+x = year * 12
+y = year * 365
+z = year * 365 * 24
+p = year * 365 * 24 * 60
+q = year * 365 * 24 * 3600
+
+print x, "months", y, "days", z, "hours", p, "minutes and", q, "seconds"
