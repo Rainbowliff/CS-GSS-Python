9a74ce97d7e518a4b329f6bae5f176c141671020
Rainbow Li <52395053+Rainbowliff@users.noreply.github.com>
2019-07-04 11:55:53 +0800

    Update Age.py
    It calculates your age ~

--- Age.py
+++ Age.py
@@ -1,10 +1,10 @@
 print ("enter your age:")
 year = input()
 
-x = year * 12
-y = year * 365
-z = year * 365 * 24
-p = year * 365 * 24 * 60
-q = year * 365 * 24 * 3600
+x = year * 12 #months
+y = year * 365 #days
+z = year * 365 * 24 #hours
+p = year * 365 * 24 * 60 #minutes
+q = year * 365 * 24 * 3600 #seconds
 
-print x, "months", y, "days", z, "hours", p, "minutes and", q, "seconds"
+print x, "months or", y, "days or", z, "hours or", p, "minutes or", q, "seconds"
