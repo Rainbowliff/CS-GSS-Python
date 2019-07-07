235a5b0e90fc891e7e8589312502ce0e0b35dd2c
Rainbow Li <52395053+Rainbowliff@users.noreply.github.com>
2019-07-07 21:56:53 +0800

    Add files via upload

---
+++ my_search.py
@@ -0,0 +1,15 @@
+my_list = [3, 15, 1, 8, 20, 7, 21, 25, 86, 198, 75, 46, 52]
+
+x = int(input("What is the number you're searching for:"))
+
+
+for number in my_list:
+    if x == number:
+        print("The index of your number is", my_list.index(x))
+        e = 0
+        break
+    else:
+        e = -1
+
+if e == -1:
+    print("The item is not found")
