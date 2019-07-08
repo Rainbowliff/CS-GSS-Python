7a04fe27ede51c2bdd50ac5822d4c093c66164d8
Rainbow Li <52395053+Rainbowliff@users.noreply.github.com>
2019-07-08 10:40:39 +0800

    Update 7.5_my_sort.py

--- 7.5_my_sort.py
+++ 7.5_my_sort.py
@@ -1,15 +1,18 @@
 my_list = [21, 7, 12, 25]
-my_list2 = []
-print(my_list)
-print("Sorting...")
-ln = len(my_list)
 
 
-while len(my_list2) < ln:
-    x = min(my_list)
-    my_list2.append(x)
-    my_list.remove(x)
+def sort():
+    my_list2 = []
+    print(my_list)
+    print("Sorting...")
+    ln = len(my_list)
+
+    while len(my_list2) < ln:
+        x = min(my_list)
+        my_list2.append(x)
+        my_list.remove(x)
+
+    print(my_list2)
 
 
-print(my_list2)
-
+sort()
