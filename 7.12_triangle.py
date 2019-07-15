6cb23fc4391399c4e40620a784280a9ffcfafb66
Rainbow Li <52395053+Rainbowliff@users.noreply.github.com>
2019-07-15 12:34:30 +0800

    Update 7.12_triangle.py

--- 7.12_triangle.py
+++ 7.12_triangle.py
@@ -5,32 +5,26 @@ class triangle:
         self.side2 = 0
         self.side3 = 0
 
+    def read_triangle(self):
+        self.side1 = int(input("Enter the length of side 1:"))
+        self.side2 = int(input("Enter the length of side 2:"))
+        self.side3 = int(input("Enter the length of side 3:"))
 
-def read_triangle(y):
-    print("Please enter the length of", y, ':')
-    x = int(input())
-    return x
+    def is_triangle(self):
+        my_list = [self.side1, self.side2, self.side3]
+        maxi = [max(my_list)]
+        my_list.remove(max(my_list))
+        if maxi[0] < my_list[0] + my_list[1]:
+            print("This is a triangle")
+            return
+        elif maxi[0] > my_list[0] + my_list[1]:
+            print("This is not a triangle")
+            return
+        elif maxi[0] == my_list[0] + my_list[1]:
+            print("This is not a triangle")
+            return
 
 
 triangle1 = triangle()
-triangle1.side1 = read_triangle('side 1')
-triangle1.side2 = read_triangle('side 2')
-triangle1.side3 = read_triangle('side 3')
-
-
-def is_triangle(a, b, c):
-    my_list = [a, b, c]
-    maxi = [max(my_list)]
-    my_list.remove(max(my_list))
-    if maxi[0] < my_list[0] + my_list[1]:
-        print("This is a triangle")
-        return
-    elif maxi[0] > my_list[0] + my_list[1]:
-        print("This is not a triangle")
-        return
-    elif maxi[0] == my_list[0] + my_list[1]:
-        print("This is not a triangle")
-        return
-
-
-is_triangle(triangle1.side1, triangle1.side2, triangle1.side3)
+triangle.read_triangle(triangle1)
+triangle.is_triangle(triangle1)
