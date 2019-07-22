88ab106dc7675af243838e1dc4bd97ea2b39fadc
Rainbow Li <52395053+Rainbowliff@users.noreply.github.com>
2019-07-22 11:26:57 +0800

    Update 7.19_Rectangle.py

--- 7.19_Rectangle.py
+++ 7.19_Rectangle.py
@@ -3,16 +3,15 @@ class rectangle:
     def __init__(self):
         self.width = 0
         self.length = 0
-        self.value = ()
 
     def __str__(self):
-        return "\nThis is a rectangle.\n\nWidth:%d\nLength:%d\nArea:%d\nPerimeter:%d" % \
-               (self.width, self.length, self.value[0], self.value[1])
+        return "\nThis is a rectangle.\n\nWidth:%d\nLength:%d\n" % \
+               (self.width, self.length)
 
     def get_value(self):
-        x = self.width * self.length
-        y = 2 * (self.width + self.length)
-        self.value = (x, y)
+        x = int(self.width * self.length)
+        y = int(2 * (self.width + self.length))
+        return (x, y)
 
     def get_width(self):
         self.width = int(input('Enter width:'))
@@ -39,9 +38,10 @@ while user_input != 'q':
             rec1 = rectangle()
             rec1.get_width()
             rec1.get_length()
-            rec1.get_value()
+            v = rec1.get_value()
             my_list.append(rec1)
             print(rec1)
+            print("Area:", v[0], "\nPerimeter:", v[1], "\n")
 
         except ValueError as excpt:
             print(excpt)
