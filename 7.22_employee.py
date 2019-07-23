ac5f201d4a81a0e1187da9facd0c63af07d2d68d
Rainbow Li <52395053+Rainbowliff@users.noreply.github.com>
2019-07-23 20:05:14 +0800

    Add files via upload

---
+++ 7.22_employee.py
@@ -0,0 +1,52 @@
+class employee:
+    def __init__(self, fname, lname, id, dur, sal):
+        self.first_name = fname
+        self.last_name = lname
+        self.eID = id
+        self.e_duration = int(dur)
+        self.salary_per_day = int(sal)
+
+    def __str__(self):
+        return "\nName:%s%s\nID:%08d\nDuration:%days\nSalary(per day):$%d" % \
+               (self.first_name, self.last_name, self.eID, self.e_duration, self.salary_per_day)
+
+    def money(self):
+        m = self.e_duration * self.salary_per_day
+        return int(m)
+
+
+file = open("7.22_input.txt", 'r')
+afile = open("7.22_output.txt", 'w')
+my_list = []
+employee_list = []
+for i in file:
+    lists = i.split(',')
+    namel = lists[0]
+    namef = lists[1]
+    eid = int(lists[2])
+    edur = int(lists[3])
+    esal = int(lists[4])
+    emp = employee(namef, namel, eid, edur, esal)
+    d = emp.money()
+    my_list.append(d)
+    employee_list.append(emp)
+    afile.write(namel)
+    afile.write(",")
+    afile.write(namef)
+    afile.write(", ")
+    afile.write(str(eid))
+    afile.write(", ")
+    afile.write(str(edur))
+    afile.write(", ")
+    afile.write(str(esal))
+    afile.write(", ")
+    afile.write(str(d))
+    afile.write("\n")
+afile.write("The company has paid all of it's employees $")
+afile.write(str(sum(my_list)))
+afile.close()
+file.close()
+nfile = open('7.22_output.txt', 'r')
+for l in nfile:
+    print(l)
+
